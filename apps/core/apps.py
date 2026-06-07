3ca7b54a2277ba647bd72a41b425748f341c47d0
rkdwjdwnsdl <rkdwjdwnsdl@gmail.com>
2026-06-07 19:42:03 +0900

    fix: apps.py name 경로 수정

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -1,5 +1,8 @@
+# apps/core/apps.py
 from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    
+    # ✅ settings.py의 'apps.core'와 반드시 일치해야 에러가 나지 않습니다.
+    name = 'apps.core'
