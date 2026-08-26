57a527db82cb7b9b6f4d814d5ccfca9e467ec7fd
rkdwjdwns <rkdwjdwns@gmail.com>
2026-08-26 14:31:00 +0900

    fix: 프론트 추가 수정2

--- apps/chatbot/admin.py
+++ apps/chatbot/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ChatMessage
 
-# Register your models here.
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'user', 'message_preview', 'response_preview', 'created_at')
+    list_filter   = ('created_at', 'user')
+    search_fields = ('message', 'response', 'user__username')
+    readonly_fields = ('created_at',)
+    ordering = ('-created_at',)
+
+    def message_preview(self, obj):
+        return (obj.message[:40] + '…') if len(obj.message) > 40 else obj.message
+    message_preview.short_description = '질문'
+
+    def response_preview(self, obj):
+        return (obj.response[:40] + '…') if len(obj.response) > 40 else obj.response
+    response_preview.short_description = '응답'
